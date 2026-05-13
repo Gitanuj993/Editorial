a1bce26944bdab5337f42245172b991613b9dde5
Gitanuj993 <anujtawar42@gmail.com>
2026-05-13 14:43:30 +0530

    zig zag 2s arr solution

---
+++ LeetCode/zig_zag_2d_array_1.py
@@ -0,0 +1,47 @@
+#m zig zag string retur leetcode problem
+def zig_zag(s:str,rows:int)-> str :
+	if rows ==  1 :
+		return s
+	# creating a           2d array
+	arr = []
+	print(" arr :	", arr)
+	for i in range(rows):
+		arr.append([])		
+	#print(" arr :	", arr)
+	# now putting values into the 2d array
+	i = 0	
+	isDown = True
+	for char in s :
+		print(" char s : " , char)
+		if  isDown == True :			
+			arr[i].append(char)
+			i += 1
+			if i == rows :
+				isDown = False		
+				i -= 2
+				print(i)
+		elif isDown == False :
+				 print(" else i : " , i)
+				 arr[i].append(char)
+				 i -= 1				
+				 if i <= 0 :
+#				 	arr[i].append(char)
+				 	isDown = True				 	
+				 	
+		#print(" arr :	", arr)
+	# creating zig zag
+	print(arr)
+	res = ""
+	for i in arr :
+		for j in i :
+			res += j
+	return res
+				
+	
+test = "ABCDEF"	
+print("test is : ", test)
+rows = 2
+res = zig_zag(test,rows)
+print("result is : ",res)
+	
+	
